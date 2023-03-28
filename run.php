2163251b60b0a54b1ea1faaf9ccbfa57d9af7e13
m <m@m-air.local>
2023-03-28 09:37:11 -0700

    Updated with env file basically, included in gitignore

--- run.php
+++ run.php
@@ -1,5 +1,8 @@
 <?php
-var_dump("Running from docker with php!");
+
+require 'env-config.php'; # mockup env file which lists variables and included in .gitignore
+
+var_dump("$APP_ENV is from the ENV file. Running from docker with php!");
 
 exec("php -S 0.0.0.0:8080");
 
